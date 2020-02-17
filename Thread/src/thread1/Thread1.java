5d8fd30d4dbd66a77bc3281a2d81f2236d930b39
barbaduomo.marco <barbaduomo.marco@BIBLIO-PC04.OSDBSESTO.local>
2020-02-17 13:41:12 +0100

    nuovo

--- Thread/src/thread1/Thread1.java
+++ Thread/src/thread1/Thread1.java
@@ -4,20 +4,20 @@ public class Thread1 {
 
     
     public static void main(String[] args) {
-        T t= new T('a');
-        t.start();
-        
-        T t2= new T('b');
-        t2.start();
-        
-        
-        R r= new R('c');
-        Thread t3 = new Thread(r);
-        t3.start();
-        
-        R r2= new R('d');
-        Thread t4 = new Thread(r2);
-        t4.start();
+//        T t= new T('a');
+//        t.start();
+//        
+//        T t2= new T('b');
+//        t2.start();
+//        
+//        
+//        R r= new R('c');
+//        Thread t3 = new Thread(r);
+//        t3.start();
+//        
+//        R r2= new R('d');
+//        Thread t4 = new Thread(r2);
+//        t4.start();
 
     }
 }
